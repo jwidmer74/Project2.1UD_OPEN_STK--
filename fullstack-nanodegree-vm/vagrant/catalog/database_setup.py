83a77e09651594c3c51931dbc7551d110762bc3f
Joshua Widmer <jakron74@hotmail.com>
2019-04-19 15:38:33 -0700

    git start over

--- fullstack-nanodegree-vm/vagrant/catalog/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/catalog/database_setup.py
@@ -8,33 +8,31 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Zoo(Base):
-    __tablename__ = 'zoo'
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
-    zoo_name = Column(String(250), nullable=False)
-    contact = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
 
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-            'zoo_name': self.zoo_name,
+            'name': self.name,
             'id': self.id,
-            'contact': self.contact,
         }
 
 
-class RescueAnimals(Base):
-    __tablename__ = 'animal'
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    species = Column(String(20))
-    diet = Column(String(250))
-    zoo_id = Column(Integer, ForeignKey('zoo.id'))
-    zoo = relationship(Zoo)
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
     @property
     def serialize(self):
@@ -43,12 +41,12 @@ class RescueAnimals(Base):
             'name': self.name,
             'description': self.description,
             'id': self.id,
-            'species': self.species,
-            'diet': self.diet,
+            'price': self.price,
+            'course': self.course,
         }
 
 
-engine = create_engine('sqlite:///jwrescueanimals.db')
+engine = create_engine('sqlite:///restaurantmenu.db')
 
 
 Base.metadata.create_all(engine)

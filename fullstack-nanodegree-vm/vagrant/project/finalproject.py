f83e513c08fd58fc22e51e110aa7a8932d51c262
Joshua Widmer <jakron74@hotmail.com>
2019-04-19 13:17:17 -0700

    updated working

--- fullstack-nanodegree-vm/vagrant/project/finalproject.py
+++ fullstack-nanodegree-vm/vagrant/project/finalproject.py
@@ -1,175 +1,165 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+from database_setup import Base, Zoo, Animal
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///zoomenu.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# Fake Restaurants
-# restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-# restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
 
-# Fake Menu Items
-# items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-# item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-# items = []
+@app.route('/zoo/<int:zoo_id>/menu/JSON')
+def zooMenuJSON(zoo_id):
+    zoo = session.query(Zoo).filter_by(id=zoo_id).one()
+    items = session.query(Animal).filter_by(
+        zoo_id=zoo_id).all()
+    return jsonify(Animals=[i.serialize for i in items])
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-def restaurantMenuJSON(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
-    return jsonify(MenuItems=[i.serialize for i in items])
-
-
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+@app.route('/zoo/<int:zoo_id>/menu/<int:zoo_id>/JSON')
+def animalJSON(zoo_id, zoo_id):
+    Menu_Item = session.query(Animal).filter_by(id=zoo_id).one()
     return jsonify(Menu_Item=Menu_Item.serialize)
 
 
-@app.route('/restaurant/JSON')
-def restaurantsJSON():
-    restaurants = session.query(Restaurant).all()
-    return jsonify(restaurants=[r.serialize for r in restaurants])
+@app.route('/zoo/JSON')
+def zoosJSON():
+    zoos = session.query(Zoo).all()
+    return jsonify(zoos=[r.serialize for r in zoos])
 
 
-# Show all restaurants
+# Show all zoos
 @app.route('/')
-@app.route('/restaurant/')
-def showRestaurants():
-    restaurants = session.query(Restaurant).all()
-    # return "This page will show all my restaurants"
-    return render_template('restaurants.html', restaurants=restaurants)
+@app.route('/zoo/')
+def showzoos():
+    zoos = session.query(Zoo).all()
+    # return "This page will show all my zoos"
+    return render_template('zoos.html', zoos=zoos)
 
 
-# Create a new restaurant
-@app.route('/restaurant/new/', methods=['GET', 'POST'])
-def newRestaurant():
+# Create a new zoo
+@app.route('/zoo/new/', methods=['GET', 'POST'])
+def newZoo():
     if request.method == 'POST':
-        newRestaurant = Restaurant(name=request.form['name'])
-        session.add(newRestaurant)
+        newZoo = Zoo(name=request.form['name'])
+        session.add(newZoo)
         session.commit()
-        return redirect(url_for('showRestaurants'))
+        return redirect(url_for('showZoos'))
     else:
-        return render_template('newRestaurant.html')
-    # return "This page will be for making a new restaurant"
+        return render_template('newZoo.html')
+    # return "This page will be for making a new zoo"
 
-# Edit a restaurant
+# Edit a zoo
 
 
-@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
-def editRestaurant(restaurant_id):
-    editedRestaurant = session.query(
-        Restaurant).filter_by(id=restaurant_id).one()
+@app.route('/zoo/<int:zoo_id>/edit/', methods=['GET', 'POST'])
+def editZoo(zoo_id):
+    editedZoo = session.query(
+        Zoo).filter_by(id=zoo_id).one()
     if request.method == 'POST':
         if request.form['name']:
-            editedRestaurant.name = request.form['name']
-            return redirect(url_for('showRestaurants'))
+            editedZoo.name = request.form['name']
+            return redirect(url_for('showZoos'))
     else:
         return render_template(
-            'editRestaurant.html', restaurant=editedRestaurant)
+            'editZoo.html', zoo=editedZoo)
 
-    # return 'This page will be for editing restaurant %s' % restaurant_id
+    # return 'This page will be for editing zoo %s' % zoo_id
 
-# Delete a restaurant
+# Delete a zoo
 
 
-@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
-def deleteRestaurant(restaurant_id):
-    restaurantToDelete = session.query(
-        Restaurant).filter_by(id=restaurant_id).one()
+@app.route('/zoo/<int:zoo_id>/delete/', methods=['GET', 'POST'])
+def deleteZoo(zoo_id):
+    zooToDelete = session.query(
+        Zoo).filter_by(id=zoo_id).one()
     if request.method == 'POST':
-        session.delete(restaurantToDelete)
+        session.delete(zooToDelete)
         session.commit()
         return redirect(
-            url_for('showRestaurants', restaurant_id=restaurant_id))
+            url_for('showZoos', zoo_id=zoo_id))
     else:
         return render_template(
-            'deleteRestaurant.html', restaurant=restaurantToDelete)
-    # return 'This page will be for deleting restaurant %s' % restaurant_id
+            'deleteZoo.html', zoo=zooToDelete)
+    # return 'This page will be for deleting zoo %s' % zoo_id
 
 
-# Show a restaurant menu
-@app.route('/restaurant/<int:restaurant_id>/')
-@app.route('/restaurant/<int:restaurant_id>/menu/')
-def showMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
-    return render_template('menu.html', items=items, restaurant=restaurant)
-    # return 'This page is the menu for restaurant %s' % restaurant_id
+# Show a zoo menu
+@app.route('/zoo/<int:zoo_id>/')
+@app.route('/zoo/<int:zoo_id>/menu/')
+def showAnimal(zoo_id):
+    zoo = session.query(Zoo).filter_by(id=zoo_id).one()
+    items = session.query(Animal).filter_by(
+        zoo_id=zoo_id).all()
+    return render_template('menu.html', items=items, zoo=zoo)
+    # return 'This page is the menu for zoo %s' % zoo_id
 
 # Create a new menu item
 
 
 @app.route(
-    '/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
-def newMenuItem(restaurant_id):
+    '/zoo/<int:zoo_id>/menu/new/', methods=['GET', 'POST'])
+def newAnimal(zoo_id):
     if request.method == 'POST':
-        newItem = MenuItem(name=request.form['name'], description=request.form[
-                           'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
+        newItem = Animal(name=request.form['name'], species=request.form[
+                           'species'], expense=request.form['expense'], diet=request.form['diet'], zoo_id=zoo_id)
         session.add(newItem)
         session.commit()
 
-        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+        return redirect(url_for('showAnimal', zoo_id=zoo_id))
     else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+        return render_template('newanimal.html', zoo_id=zoo_id)
 
-    return render_template('newMenuItem.html', restaurant=restaurant)
-    # return 'This page is for making a new menu item for restaurant %s'
-    # %restaurant_id
+    return render_template('newAnimal.html', zoo=zoo)
+    # return 'This page is for making a new menu item for zoo %s'
+    # %zoo_id
 
 # Edit a menu item
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit',
+@app.route('/zoo/<int:zoo_id>/menu/<int:zoo_id>/edit',
            methods=['GET', 'POST'])
-def editMenuItem(restaurant_id, menu_id):
-    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+def editAnimal(zoo_id, zoo_id):
+    editedItem = session.query(Animal).filter_by(id=zoo_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
-        if request.form['description']:
-            editedItem.description = request.form['name']
-        if request.form['price']:
-            editedItem.price = request.form['price']
-        if request.form['course']:
-            editedItem.course = request.form['course']
+        if request.form['species']:
+            editedItem.species = request.form['name']
+        if request.form['expense']:
+            editedItem.expense = request.form['expense']
+        if request.form['diet']:
+            editedItem.diet = request.form['diet']
         session.add(editedItem)
         session.commit()
-        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+        return redirect(url_for('showAnimal', zoo_id=zoo_id))
     else:
 
         return render_template(
-            'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
+            'editAnimal.html', zoo_id=zoo_id, zoo_id=zoo_id, item=editedItem)
 
-    # return 'This page is for editing menu item %s' % menu_id
+    # return 'This page is for editing menu item %s' % zoo_id
 
 # Delete a menu item
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete',
+@app.route('/zoo/<int:zoo_id>/menu/<int:zoo_id>/delete',
            methods=['GET', 'POST'])
-def deleteMenuItem(restaurant_id, menu_id):
-    itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
+def deleteAnimal(zoo_id, zoo_id):
+    itemToDelete = session.query(Animal).filter_by(id=zoo_id).one()
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
-        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+        return redirect(url_for('showAnimal', zoo_id=zoo_id))
     else:
-        return render_template('deleteMenuItem.html', item=itemToDelete)
-    # return "This page is for deleting menu item %s" % menu_id
+        return render_template('deleteAnimal.html', item=itemToDelete)
+    # return "This page is for deleting menu item %s" % zoo_id
 
 
 if __name__ == '__main__':
--- fullstack-nanodegree-vm/vagrant/project/loaddata.py
+++ fullstack-nanodegree-vm/vagrant/project/loaddata.py
@@ -26,56 +26,56 @@ session.add(zoo1)
 session.commit()
 
 animal2 = Animal(name="Donkey", species="Equus africanus asinus",
-                     diet="hay", description="", zoo=zoo1)
+                     diet="hay", species="", zoo=zoo1)
 
 session.add(animal2)
 session.commit()
 
 
 animal1 = Animal(name="sloth", species="Pilosa",
-                     diet="leaves", description="", zoo=zoo1)
+                     diet="leaves", species="", zoo=zoo1)
 
 session.add(animal1)
 session.commit()
 
 animal2 = Animal(name="sea horse", species="Hippocampus",
-                     diet="plankton", description="", zoo=zoo1)
+                     diet="plankton", species="", zoo=zoo1)
 
 session.add(animal2)
 session.commit()
 
 animal3 = Animal(name="parrot fish", species="Chordate",
-                     diet="fish", description="", zoo=zoo1)
+                     diet="fish", species="", zoo=zoo1)
 
 session.add(animal3)
 session.commit()
 
 animal4 = Animal(name="nuteria", species="Myocastor coypus",
-                     diet="anything", description="", zoo=zoo1)
+                     diet="anything", species="", zoo=zoo1)
 
 session.add(animal4)
 session.commit()
 
 animal5 = Animal(name="wood duck", species="Aix sponsa",
-                     diet="duck weed", description="", zoo=zoo1)
+                     diet="duck weed", species="", zoo=zoo1)
 
 session.add(animal5)
 session.commit()
 
 animal6 = Animal(name="giraffe", species="Giraffa camelopardalis",
-                     diet="tree leaves", description="", zoo=zoo1)
+                     diet="tree leaves", species="", zoo=zoo1)
 
 session.add(animal6)
 session.commit()
 
 animal7 = Animal(name="chimpanzee", species="Pan troglodytes",
-                     diet="bananas", description="", zoo=zoo1)
+                     diet="bananas", species="", zoo=zoo1)
 
 session.add(animal7)
 session.commit()
 
 animal8 = Animal(name="falcon", species="Falco peregrinus",
-                     diet="rodents", description="", zoo=zoo1)
+                     diet="rodents", species="", zoo=zoo1)
 
 session.add(animal8)
 session.commit()
@@ -91,31 +91,31 @@ session.commit()
 
 
 animal1 = Animal(name="reindeer", species="Rangifer tarandus",
-                     diet="Santas Cookies", description="", zoo=zoo1)
+                     diet="Santas Cookies", species="", zoo=zoo1)
 
 session.add(animal1)
 session.commit()
 
 animal2 = Animal(name="alligator turtle", species=" Macrochelys temminckii",
-                     diet="lettus", description="", zoo=zoo1)
+                     diet="lettus", species="", zoo=zoo1)
 
 session.add(animal2)
 session.commit()
 
 animal3 = Animal(name="grizzly bear", species=" Ursus arctos horribilis",
-                     diet="Just about anything", description="", zoo=zoo1)
+                     diet="Just about anything", species="", zoo=zoo1)
 
 session.add(animal3)
 session.commit()
 
 animal4 = Animal(name="stork", species="Ciconiidae",
-                     diet="fish", description="", zoo=zoo1)
+                     diet="fish", species="", zoo=zoo1)
 
 session.add(animal4)
 session.commit()
 
 animal2 = Animal(name="snapping turtle", species=" Chelydra serpentina",
-                     diet="leaves", description="", zoo=zoo1)
+                     diet="leaves", species="", zoo=zoo1)
 
 session.add(animal2)
 session.commit()
@@ -129,43 +129,43 @@ session.commit()
 
 
 animal1 = Animal(name="anteater", species="Vermilingua",
-                     diet="ants", description="", zoo=zoo1)
+                     diet="ants", species="", zoo=zoo1)
 
 session.add(animal1)
 session.commit()
 
 animal2 = Animal(name="black bear", species="Ursus americanus",
-                     diet="picnic baskets", description="", zoo=zoo1)
+                     diet="picnic baskets", species="", zoo=zoo1)
 
 session.add(animal2)
 session.commit()
 
 animal3 = Animal(name="african elephant", species=" Elephantidae",
-                     diet="leaves and bark", description="", zoo=zoo1)
+                     diet="leaves and bark", species="", zoo=zoo1)
 
 session.add(animal3)
 session.commit()
 
 animal4 = Animal(name="porcupine", species="Rodent",
-                     diet="pizza", description="", zoo=zoo1)
+                     diet="pizza", species="", zoo=zoo1)
 
 session.add(animal4)
 session.commit()
 
 animal5 = Animal(name="flamingo", species="Phoenicopterus",
-                     diet="shimp", description="", zoo=zoo1)
+                     diet="shimp", species="", zoo=zoo1)
 
 session.add(animal5)
 session.commit()
 
 animal6 = Animal(name="camel", species="Camelus",
-                     diet="hay", description="", zoo=zoo1)
+                     diet="hay", species="", zoo=zoo1)
 
 session.add(animal6)
 session.commit()
 
 animal7 = Animal(name="alpaca", species="Camelus",
-                     diet="hay", description="", zoo=zoo1)
+                     diet="hay", species="", zoo=zoo1)
 
 session.add(animal7)
 session.commit()

c670bba1d8a05015136ac4a1ad47a475d458eb14
Joshua Widmer <jakron74@hotmail.com>
2019-05-01 12:07:58 -0700

    got only first item right

--- fullstack-nanodegree-vm/vagrant/catalog/application.py
+++ fullstack-nanodegree-vm/vagrant/catalog/application.py
@@ -29,8 +29,7 @@ session = DBSession()
 @app.route('/category/<int:category_id>/menu/JSON')
 def categoryMenuJSON(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
-    items = session.query(Item).filter_by(
-        category_id=category_id).all()
+    items = session.query(Item).filter_by(category_id=category_id).all()
     return jsonify(Items=[i.serialize for i in items])
 
 
@@ -38,8 +37,7 @@ def categoryMenuJSON(category_id):
 def itemJSON(category_id, menu_id):
     Item = session.query(Item).filter_by(id=menu_id).one()
     return jsonify(Item=Item.serialize)
-
-
+	
 @app.route('/category/JSON')
 def categoriesJSON():
     categories = session.query(Category).all()
@@ -50,15 +48,11 @@ def categoriesJSON():
 @app.route('/')
 @app.route('/category/')
 def showCategories():
-	
     categories = session.query(Category).all()
-    # return "This page will show all my categories"
-    return render_template('categories.html', categories=categories)
-@app.route('/')
-def showLatestMenuItems():
-	conn = engine.connect()
-    latestItems = conn.execute(select([name.item])).scalar()
-    return render_template('categories.html',latestItems=latestItems)
+    conn = engine.connect()
+    
+    latestItems = conn.execute(select([Item.name])).scalar()
+    return render_template('categories.html',latestItems=latestItems,categories=categories)
 
 # Create a new category
 @app.route('/category/new/', methods=['GET', 'POST'])
@@ -70,32 +64,29 @@ def newCategory():
         return redirect(url_for('showCategories'))
     else:
         return render_template('newCategory.html')
-    # return "This page will be for making a new category"
+# return "This page will be for making a new category"
 
 # Edit a category
 
 
 @app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
 def editCategory(category_id):
-    editedCategory = session.query(
-        Category).filter_by(id=category_id).one()
+    editedCategory = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedCategory.name = request.form['name']
             return redirect(url_for('showCategories'))
     else:
-        return render_template(
-            'editCategory.html', category=editedCategory)
+        return render_template('editCategory.html', category=editedCategory)
 
-    # return 'This page will be for editing category %s' % category_id
+# return 'This page will be for editing category %s' % category_id
 
 # Delete a category
 
 
 @app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
-    categoryToDelete = session.query(
-        Category).filter_by(id=category_id).one()
+    categoryToDelete = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         session.delete(categoryToDelete)
         session.commit()
@@ -112,19 +103,16 @@ def deleteCategory(category_id):
 @app.route('/category/<int:category_id>/menu/')
 def showMenu(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
-    items = session.query(Item).filter_by(
-        category_id=category_id).all()
+    items = session.query(Item).filter_by(category_id=category_id).all()
     return render_template('menu.html', items=items, category=category)
     # return 'This page is the menu for category %s' % category_id
 
-# Create a new menu item
+	# Create a new menu item
 
-@app.route(
-    '/category/<int:category_id>/menu/new/', methods=['GET', 'POST'])
+@app.route('/category/<int:category_id>/menu/new/', methods=['GET', 'POST'])
 def newItem(category_id):
     if request.method == 'POST':
-        newItem = Item(name=request.form['name'], description=request.form[
-                           'description'], price=request.form['price'], course=request.form['course'], category_id=category_id)
+        newItem = Item(name=request.form['name'], description=request.form['description'], price=request.form['price'], course=request.form['course'], category_id=category_id)
         session.add(newItem)
         session.commit()
 
@@ -136,7 +124,7 @@ def newItem(category_id):
     # return 'This page is for making a new menu item for category %s'
     # %category_id
 
-# Edit a menu item
+	# Edit a menu item
 
 
 @app.route('/category/<int:category_id>/menu/<int:menu_id>/edit',
@@ -160,13 +148,12 @@ def editItem(category_id, menu_id):
         return render_template(
             'editItem.html', category_id=category_id, menu_id=menu_id, item=editedItem)
 
-    # return 'This page is for editing menu item %s' % menu_id
+# return 'This page is for editing menu item %s' % menu_id
 
 # Delete a menu item
 
 
-@app.route('/category/<int:category_id>/menu/<int:menu_id>/delete',
-           methods=['GET', 'POST'])
+@app.route('/category/<int:category_id>/menu/<int:menu_id>/delete',methods=['GET', 'POST'])
 def deleteItem(category_id, menu_id):
     itemToDelete = session.query(Item).filter_by(id=menu_id).one()
     if request.method == 'POST':
@@ -175,7 +162,7 @@ def deleteItem(category_id, menu_id):
         return redirect(url_for('showMenu', category_id=category_id))
     else:
         return render_template('deleteItem.html', item=itemToDelete)
-    # return "This page is for deleting menu item %s" % menu_id
+		# return "This page is for deleting menu item %s" % menu_id
 
 
 if __name__ == '__main__':

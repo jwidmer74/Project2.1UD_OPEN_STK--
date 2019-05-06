02d3d656aca4ebda3d487c13f0d925e2c34fe2db
Joshua Widmer <jakron74@hotmail.com>
2019-05-06 07:31:27 -0700

    oauth giving token need link still

--- fullstack-nanodegree-vm/vagrant/catalog/application.py
+++ fullstack-nanodegree-vm/vagrant/catalog/application.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import Flask, render_template, request, redirect, jsonify, url_for, abort, g,make_response
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,relationship
 from sqlalchemy.sql import select
-from database_setup2 import Base, Category, Item
+from database_setup2 import Base,Category,Item,User
+from flask.ext.httpauth import HTTPBasicAuth
+import json
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+ 
+import requests
 
 app = Flask(__name__)
-
+auth = HTTPBasicAuth()
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
@@ -14,16 +22,16 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# Fake categories
-# category = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-# categories = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+app = Flask(__name__)
 
 
-# Fake Menu Items
-# items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-# item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-# items = []
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+
+
+
+
+
 
 
 @app.route('/category/<int:category_id>/menu/JSON')
@@ -43,6 +51,14 @@ def categoriesJSON():
     categories = session.query(Category).all()
     return jsonify(categories=[r.serialize for r in categories])
 	
+@app.route('/categoryauth/')
+def showAuthCategories():
+    categories = session.query(Category).all()
+    conn = engine.connect()
+    
+    #latestItems = conn.execute(select([Item.name])).scalar()
+    latestItems = session.query((select([Item.name]).order_by((Item.name).desc()).limit(10))).all()
+    return render_template('categoriesauth.html',latestItems=latestItems,categories=categories)	
 
 # Show all categories
 @app.route('/')
@@ -100,7 +116,17 @@ def deleteCategory(category_id):
             'deleteCategory.html', category=categoryToDelete)
     # return 'This page will be for deleting category %s' % category_id
 
+# Show a category menu
+@app.route('/categoryauth/<int:category_id>/')
+@app.route('/categoryauth/<int:category_id>/menu/')
+def showMenuAuth(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id=category_id).all()
+    return render_template('menuAuth.html', items=items, category=category)
+    # return 'This page is the menu for category %s' % category_id
 
+	# Create a new menu item
+	
 # Show a category menu
 @app.route('/category/<int:category_id>/')
 @app.route('/category/<int:category_id>/menu/')
@@ -166,7 +192,136 @@ def deleteItem(category_id, menu_id):
         return render_template('deleteItem.html', item=itemToDelete)
 		# return "This page is for deleting menu item %s" % menu_id
 
+@auth.verify_password
+def verify_password(username_or_token, password):
+    #Try to see if it's a token first
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id = user_id).one()
+    else:
+        user = session.query(User).filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
 
+@app.route('/login')
+def start():
+    return render_template('login.html')
+
+@app.route('/login/<provider>', methods = ['POST'])
+def login(provider):
+    #STEP 1 - Parse the auth code
+    auth_code = request.json.get('auth_code')
+    print "Step 1 - Complete, received auth code %s" % auth_code
+    if provider == 'google':
+        #STEP 2 - Exchange for a token
+        try:
+            # Upgrade the authorization code into a credentials object
+            oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+            oauth_flow.redirect_uri = 'postmessage'
+            credentials = oauth_flow.step2_exchange(auth_code)
+        except FlowExchangeError:
+            response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+          
+        # Check that the access token is valid.
+        access_token = credentials.access_token
+        url = ('https://www.googleapis.com/login2/v1/tokeninfo?access_token=%s' % access_token)
+        h = httplib2.Http()
+        result = json.loads(h.request(url, 'GET')[1])
+
+        if result.get('error') is not None:
+            response = make_response(json.dumps(result.get('error')), 500)
+            response.headers['Content-Type'] = 'application/json'
+            
+
+        print "Step 2 Complete! Access Token : %s " % credentials.access_token
+
+        #STEP 3 - Find User or make a new one
+        
+        #Get user info
+        h = httplib2.Http()
+        userinfo_url =  "https://www.googleapis.com/login2/v1/userinfo"
+        params = {'access_token': credentials.access_token, 'alt':'json'}
+        answer = requests.get(userinfo_url, params=params)
+      
+        data = answer.json()
+
+        name = data['name']
+        picture = data['picture']
+        email = data['email']
+        
+        
+     
+        #see if user exists, if it doesn't make a new one
+        user = session.query(User).filter_by(email=email).first()
+        if not user:
+            user = User(username = name, picture = picture, email = email)
+            session.add(user)
+            session.commit()
+
+        
+
+        #STEP 4 - Make token
+        token = user.generate_auth_token(600)
+
+        
+
+        #STEP 5 - Send back token to the client 
+        return jsonify({'token': token.decode('ascii')})
+        
+        #return jsonify({'token': token.decode('ascii'), 'duration': 600})
+    else:
+        return 'Unrecoginized Provider'
+
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+
+
+@app.route('/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        print "missing arguments"
+        abort(400) 
+        
+    if session.query(User).filter_by(username = username).first() is not None:
+        print "existing user"
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
+        
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = session.query(User).filter_by(id=id).one()
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    app.run(host='0.0.0.0', port=5000)
+	
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
